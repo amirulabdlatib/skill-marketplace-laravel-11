4dfe8fe144e83584b82d6d2d77e57437b35b076a
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-27 22:20:45 +0800

    Setup preline

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,11 +3,13 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  @vite(['resources/css/app.css','resources/css/app.js'])
 </head>
 <body>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+
+<script src="./node_modules/preline/dist/preline.js"></script>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
 
