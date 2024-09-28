4804ac937a3966bae5780bf6a80ca89bae396484
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-28 13:14:45 +0800

    Add landing page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,15 +1,21 @@
 <!doctype html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/css/app.css','resources/css/app.js'])
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
 
-<script src="./node_modules/preline/dist/preline.js"></script>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('page-title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+</head>
+
+<body>
+  @include('components.navbar')
+    <main>
+        {{ $slot }}
+    </main>
+    @stack('scripts')
+
 </body>
+
 </html>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,3 +1,13 @@
+@section('page-title', 'SkillBridge | Landing Page')
+
 <x-layout>
-    
+    <section class="container mx-auto flex min-h-screen items-center justify-center px-4">
+        <div class=" max-w-4xl">
+            <h1 class="text-5xl font-bold mb-6">Where Skills Meet Projects. Your Gateway to Professional Success</h1>
+            <h2 class="text-3xl font-semibold mb-8"></h2>
+            <p class="text-xl mb-8">SkillBridge is the innovative platform bridging the gap between skilled professionals
+                and exciting projects. We're revolutionizing how talent connects with opportunity in the digital age.
+            </p>
+        </div>
+    </section>
 </x-layout>
