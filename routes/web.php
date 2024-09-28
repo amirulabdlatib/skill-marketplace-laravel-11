1fc5d4500617fa8675bbca952a0529470db96e88
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-28 19:15:04 +0800

    Add in built middleware auth and guest

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,27 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RecruiterController;
 use App\Http\Controllers\TalentController;
 
-Route::get('/',function(){
+Route::get('/', function () {
     return view('landing');
 })->name('landing');
 
-Route::get('/login',[AuthController::class,'getLoginPage'])->name('login.page');
-Route::get('/register',[AuthController::class,'getRegisterPage'])->name('register.page');
-Route::post('/register',[AuthController::class,'doRegister'])->name('register');
-Route::post('/login',[AuthController::class,'doLogin'])->name('login');
-Route::post('/logout',[AuthController::class,'doLogout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'doLogout'])->name('logout');
 
-
-Route::prefix('recruiter')->group(function(){
-    Route::get('/dashboard',[RecruiterController::class,'dashboard'])->name('recruiter.dashboard');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'getLoginPage'])->name('login.page');
+    Route::get('/register', [AuthController::class, 'getRegisterPage'])->name('register.page');
+    Route::post('/register', [AuthController::class, 'doRegister'])->name('register');
+    Route::post('/login', [AuthController::class, 'doLogin'])->name('login');
 });
 
+Route::middleware(['auth'])
+    ->prefix('recruiter')
+    ->group(function () {
+        Route::get('/dashboard', [RecruiterController::class, 'dashboard'])->name('recruiter.dashboard');
+    });
 
-Route::prefix('talent')->group(function(){
-    Route::get('/dashboard',[TalentController::class,'dashboard'])->name('talent.dashboard');
-});
+Route::middleware(['auth'])
+    ->prefix('talent')
+    ->group(function () {
+        Route::get('/dashboard', [TalentController::class, 'dashboard'])->name('talent.dashboard');
+    });
